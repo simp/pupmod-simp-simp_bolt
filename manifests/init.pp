4f7507c667968654ffa39892172fedecfdf3c47a
Michael Morrone <michael.morrone@onyxpoint.com>
2019-10-21 09:04:43 -0400

    Fixed spacing

--- manifests/init.pp
+++ manifests/init.pp
@@ -30,12 +30,12 @@
 # @author SIMP Team <https://simp-project.com/>
 #
 class simp_bolt (
-  String[1]           $target_user_name      = 'simp_bolt',
-  Stdlib::Unixpath    $target_user_home      = "/var/local/${target_user_name}",
-  Optional[String[1]] $target_sudo_user      = 'root',
-  Boolean             $bolt_controller       = false,
-  Boolean             $bolt_target           = false,
-  String              $package_name          = 'puppet-bolt'
+  String[1]           $target_user_name = 'simp_bolt',
+  Stdlib::Unixpath    $target_user_home = "/var/local/${target_user_name}",
+  Optional[String[1]] $target_sudo_user = 'root',
+  Boolean             $bolt_controller  = false,
+  Boolean             $bolt_target      = false,
+  String              $package_name     = 'puppet-bolt'
 ) {
 
   simplib::assert_metadata($module_name)
