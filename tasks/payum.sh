d25ec7a5b66ab582b68ce6d49811513849c17ac8
Michael Morrone <michael.morrone@onyxpoint.com>
2019-11-13 14:42:42 -0500

    Updated payum for case where no puppet-agent in yum

--- tasks/payum.sh
+++ tasks/payum.sh
@@ -3,16 +3,16 @@
 yum --showduplicates list puppet-agent-${PT_version} >/dev/null 2>&1
 if [ $? = 0 ]; then
   # Check for specified version and return if available
-  echo "$PT_version"
+  echo -n "$PT_version"
 else
   # Determine most recent puppet-agent version
-  version=`yum --showduplicates list puppet-agent|grep puppet-agent|awk {'print $2'}|sort --version-sort|tail -1`
-  if [ -z $version ]; then
+  version=`yum --showduplicates list puppet-agent 2>/dev/null|grep puppet-agent|awk {'print $2'}|sort --version-sort|tail -1`
+  if [ -z $version ] ; then
     # Return 0 if puppet-agent is not found in available repos
-    echo "0"
+    echo -n "0"
   else
     # Return most recent version of puppet-agent found in repos
-    echo "${version}"
+    echo -n "${version}"
   fi
 fi
 
